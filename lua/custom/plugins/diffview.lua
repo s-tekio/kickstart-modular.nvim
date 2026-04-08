07cc9d820caeef9ec124facd90f67d46a3ec7f49
Tekio <ridich@gmail.com>
2026-04-08 05:55:32 +0200

    install plugin diffview and new custom mapping to diff against local/remote branches

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,41 @@
+return {
+  'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+  opts = {},
+  keys = {
+    {
+      '<leader>gd',
+      function()
+        local locals = vim.fn.systemlist [[git branch --format="%(refname:short)"]]
+        local remotes = vim.fn.systemlist [[git branch -r --format="%(refname:short)"]]
+
+        local choices = {}
+
+        table.insert(choices, { name = '--- LOCAL BRANCHES ---', is_header = true })
+        for _, name in ipairs(locals) do
+          table.insert(choices, { name = name, icon = '', group = 'local' })
+        end
+
+        table.insert(choices, { name = '--- REMOTE BRANCHES ---', is_header = true })
+        for _, name in ipairs(remotes) do
+          if not name:find 'HEAD' then table.insert(choices, { name = name, icon = '󱓞', group = 'remote' }) end
+        end
+
+        vim.ui.select(choices, {
+          prompt = 'Diffview: Select branch to compare',
+          format_item = function(item)
+            if item.is_header then return item.name end
+            return string.format('%s %s (%s)', item.icon, item.name, item.group)
+          end,
+        }, function(choice)
+          if not choice or choice.is_header then return end
+
+          vim.cmd('DiffviewOpen ' .. choice.name)
+        end)
+      end,
+      desc = 'Git: Diff against branch',
+    },
+    { '<leader>gD', '<cmd>DiffviewOpen<cr>', desc = 'Git: Diff working tree' },
+    { '<leader>gf', '<cmd>DiffviewFileHistory %<cr>', desc = 'Git: Diff [F]ile History' },
+  },
+}
