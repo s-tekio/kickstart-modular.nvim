6ddcf1b75f36bc14ec17f5c615ee403c2b0dc71f
Tekio <ridich@gmail.com>
2026-06-10 11:29:46 +0200

    add description to neo-tree function mappings

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,7 +13,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '<leader>\\', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
     { '<leader>nr', ':Neotree reveal<CR>', desc = '[N]eoTree [R]eveal', silent = true },
     { '<leader>gs', ':Neotree float git_status<CR>', desc = '[N]eoTree [G]it [S]tatus', silent = true },
   },
@@ -24,44 +24,50 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
-          ['<leader>tt'] = function(state)
-            local node = state.tree:get_node()
-            if node == nil then return end
+          ['<leader>tt'] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node == nil then return end
 
-            local full_path = node:get_id()
-            -- Obtenemos el nombre del proyecto para recortar la ruta
-            -- Si el proyecto es 'authentication-service', queremos lo que va después
-            local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-            local relative_path = full_path:match('.*/' .. project_name .. '/(.+)') or full_path
+              local full_path = node:get_id()
+              -- Obtenemos el nombre del proyecto para recortar la ruta
+              -- Si el proyecto es 'authentication-service', queremos lo que va después
+              local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+              local relative_path = full_path:match('.*/' .. project_name .. '/(.+)') or full_path
 
-            -- Ahora llamamos a TestFile con la ruta ya limpia
-            vim.api.nvim_command('TestFile ' .. relative_path)
-          end,
-          ['<leader>fs'] = function(state)
-            local node = state.tree:get_node()
-            if node == nil then return end
+              -- Ahora llamamos a TestFile con la ruta ya limpia
+              vim.api.nvim_command('TestFile ' .. relative_path)
+            end,
+            desc = 'run_tests_in_folder',
+          },
+          ['<leader>fs'] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node == nil then return end
 
-            local full_path = node:get_id()
-            local target_dir
+              local full_path = node:get_id()
+              local target_dir
 
-            -- If it's a directory, use it directly.
-            -- If it's a file, get its parent directory.
-            if node.type == 'directory' then
-              target_dir = full_path
-            else
-              target_dir = vim.fn.fnamemodify(full_path, ':h')
-            end
+              -- If it's a directory, use it directly.
+              -- If it's a file, get its parent directory.
+              if node.type == 'directory' then
+                target_dir = full_path
+              else
+                target_dir = vim.fn.fnamemodify(full_path, ':h')
+              end
 
-            -- Launch snacks picker targeting that specific directory
-            require('snacks').picker.grep {
-              cwd = target_dir,
-              hidden = true,
-              ignored = true,
-              exclude = { '.git', 'node_modules', '**/cache/**', '**/log/**', '**/logs/**' },
-              args = { '--smart-case' },
-              title = 'Grep in ' .. vim.fn.fnamemodify(target_dir, ':t'),
-            }
-          end,
+              -- Launch snacks picker targeting that specific directory
+              require('snacks').picker.grep {
+                cwd = target_dir,
+                hidden = true,
+                ignored = true,
+                exclude = { '.git', 'node_modules', '**/cache/**', '**/log/**', '**/logs/**' },
+                args = { '--smart-case' },
+                title = 'Grep in ' .. vim.fn.fnamemodify(target_dir, ':t'),
+              }
+            end,
+            desc = 'search_in_folder',
+          },
         },
       },
     },
